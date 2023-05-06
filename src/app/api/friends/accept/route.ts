99d446885ced719be3a09f7a18e2a33f4360c174
FacundoTenuta <facu_201117@hotmail.com>
2023-05-06 15:04:43 -0300

    ⚡️ Add pusher server trigger event on new friend

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from '@/helpers/redis';
 import { authOptions } from '@/lib/auth';
 import { db } from '@/lib/db';
+import { pusherServer } from '@/lib/pusher';
+import { toPusherKey } from '@/lib/utils';
 import { getServerSession } from 'next-auth';
 import { z } from 'zod';
 
@@ -48,6 +50,12 @@ export async function POST(req: Request) {
 				statusText: 'No friend request',
 			});
 		}
+
+		pusherServer.trigger(
+			toPusherKey(`user:${idToAdd}:friends`),
+			'new_friend',
+			{}
+		);
 
 		await db.sadd(`user:${session.user.id}:friends`, idToAdd);
 
